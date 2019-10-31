adfa6b35874fdd5e95a66ef3e3d607144dda1404
Jerry1919 <48582347+Jerry1919@users.noreply.github.com>
2019-10-31 12:06:55 +0530

    improved
    assistant name can be changed in start
    word search changed into substring search in some cases

--- 0.py
+++ 0.py
@@ -2,6 +2,7 @@ import os
 import random
 
 robo = "champ"
+empty = [" ","\t","\n",""]
 
 print(f"{robo}: Hi.!! I am {robo}, here for you.")
 print(f"{robo}: What's your user name? ")
@@ -11,11 +12,23 @@ if not os.path.exists(f'{user}.txt'):
     print(f"{robo}: I'm glad that you are here 😊  I assure, you will have great experiance.")
     print(f"{robo}: {user} Please, Enter password for your new account :")
     enteredPassword = input(f"{user} : ")
-    f = open(f'{user}.txt','a')
-    f.write(f"Password : {enteredPassword} : \n")
-    f.close()
-    print(f"{robo}: your account has been created successfully.")
-    print(f"{robo}: To proceed further please login again.")
+    if not(enteredPassword in  empty):
+        f = open(f'{user}.txt','a')
+        f.write(f"Password : {enteredPassword} : \n")
+        for i in range(1,4):
+            print(f"{robo}: What do you want to call me :")
+            robo = input(f"{user} : ")
+            if not(robo in empty):
+                print(f"{robo}: I really like this name")
+                break
+        if(robo in empty):
+            robo = "champ"
+        f.write(f"Robo : {robo} : \n")
+        print(f"{robo}: your account has been created successfully.")
+        print(f"{robo}: To proceed further please signin again.")
+        f.close()
+    else:
+        print(f"{robo}: Sorry you haven't entered Password, please signup again.")
     exit()
 
 else:
@@ -25,7 +38,6 @@ else:
     f = open(f'{user}.txt','r+')
     f.seek(0)
     lines = f.readlines()
-    f.close()
     for line in lines:
         Key,Value,a1 = line.split(" : ")
         if(Key.lower() == "password"):
@@ -34,17 +46,24 @@ else:
     if not(passwordValue == enteredPassword):
         print(f"{robo}: 'WRONG PASSWORD', please try again")
         exit()
+    for line in lines:
+        Key,Value,a1 = line.split(" : ")
+        if(Key.lower() == "robo"):
+            robo = Value
+            break
+    f.close()
     print(f"{robo}: what do you want to discover?")
-    # TODO :
-    #        already saved name of robo
 
-welcomeGreetings = ["hello", "hi","hii","hiii", "greetings", "sup","hey","good morning","good evening","good afternoon"]
+
+welcomeGreetings = ["hello", "hi","hii","hiii", "greetings", "sup","hey"]
+welcomeGreetings1 = ["good morning","good evening","good afternoon"]
 welcomeGreetingResponces = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 exitGreetings = ["exit","bye","byee","byeee","quit","good night","thanks","thank you"]
 saveData = ["save","add","remember","store"]
 typesOfData = ["name","roll number","gmail","address","branch","college","birthdate","year","hostel","friends","place","TODO","mobile"]
 userCall = ["me","I","mine","my"]
 askData = ["show","tell"]
+changeData = ["change"]
 askdiff = ["what","what's"]
 
 def userInputs(robo,user,status):
@@ -55,14 +74,16 @@ def userInputs(robo,user,status):
         for word in userCall:
             if(keyword == word):
                 own = 1
-    for keyword in extractOfQuestion:
-        for word in welcomeGreetings:
-            if(keyword == word):
+    for word in welcomeGreetings1:
+            if(word in userquestion.lower()):
+                print(f"{robo} : {word.title()} buddy 😊 ")
+                userInputs(robo,user,1)
+    for word in welcomeGreetings:
+            if(word in userquestion.lower()):
                 print(f"{robo} : {random.choice(welcomeGreetingResponces)}")
                 userInputs(robo,user,1)
-    for keyword in extractOfQuestion:
-        for word in exitGreetings:
-            if(keyword == word):
+    for word in exitGreetings:
+            if(word in userquestion.lower()):
                 print(f"{robo} : Ok Byeee.! Have a nice day 😊 ")
                 exit()
     for keyword in extractOfQuestion:
