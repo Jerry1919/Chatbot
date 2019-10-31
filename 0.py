36407c983ba9bcde3f80d79129190f9385b551b4
Jerry1919 <48582347+Jerry1919@users.noreply.github.com>
2019-10-31 11:23:48 +0530

    improved

--- 0.py
+++ 0.py
@@ -4,11 +4,13 @@ import random
 robo = "champ"
 
 print(f"{robo}: Hi.!! I am {robo}, here for you.")
-user = input(f"{robo}: What's your user name? : ")
+print(f"{robo}: What's your user name? ")
+user = input(f"user : ")
 
 if not os.path.exists(f'{user}.txt'):
     print(f"{robo}: I'm glad that you are here 😊  I assure, you will have great experiance.")
-    enteredPassword = input(f"{robo}: Please, Enter password for your new account :")
+    print(f"{robo}: {user} Please, Enter password for your new account :")
+    enteredPassword = input(f"{user} : ")
     f = open(f'{user}.txt','a')
     f.write(f"Password : {enteredPassword} : \n")
     f.close()
@@ -18,12 +20,12 @@ if not os.path.exists(f'{user}.txt'):
 
 else:
     print(f"{robo}: I'm glad that you are back here again 😊")
-    enteredPassword = input(f"{robo}: Please, Enter your password : ")
+    print(f"{robo}: {user} Please, Enter your password ")
+    enteredPassword = input(f"{user}: ")
     f = open(f'{user}.txt','r+')
     f.seek(0)
     lines = f.readlines()
     f.close()
-    passwordValue = "aaaa"
     for line in lines:
         Key,Value,a1 = line.split(" : ")
         if(Key.lower() == "password"):
@@ -36,13 +38,14 @@ else:
     # TODO :
     #        already saved name of robo
 
-welcomeGreetings = ["hello", "hi","hii","hiii", "greetings", "sup", "what's up","hey","good morning","good evening","good afternoon"]
+welcomeGreetings = ["hello", "hi","hii","hiii", "greetings", "sup","hey","good morning","good evening","good afternoon"]
 welcomeGreetingResponces = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 exitGreetings = ["exit","bye","byee","byeee","quit","good night","thanks","thank you"]
 saveData = ["save","add","remember","store"]
 typesOfData = ["name","roll number","gmail","address","branch","college","birthdate","year","hostel","friends","place","TODO","mobile"]
 userCall = ["me","I","mine","my"]
-askData = {"show","tell"}
+askData = ["show","tell"]
+askdiff = ["what","what's"]
 
 def userInputs(robo,user,status):
     own = 0
@@ -75,7 +78,7 @@ def userInputs(robo,user,status):
                 userInputs(robo,user,status)
     for keyword in extractOfQuestion:
         for word in askData:
-            if(keyword == word or (keyword == "what" and own==1)):
+            if(keyword == word or ((keyword in askdiff) and own==1)):        # ME => me mechanical
                 f = open(f'{user}.txt','r')
                 f.seek(0)
                 lines = f.readlines()
@@ -88,6 +91,7 @@ def userInputs(robo,user,status):
                         userInputs(robo,user,status)                
                 print(f"{robo} : Sorry but this data is not available.")
                 userInputs(robo,user,status)
+    print(f"{robo} : Sorry but I am not supposed to answer this !")
                 
                       
 
